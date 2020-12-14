1ed8ca3cc3a1dea9c44da508a943142347bf4001
Kerndog73 <kerndog73@gmail.com>
2020-12-14 17:27:57 +1030

    Clean up day 14

--- src/bin/day_14.rs
+++ src/bin/day_14.rs
@@ -1,82 +1,99 @@
 use adventofcode2020::*;
 
-fn part_one() {
-    let mut ones_mask: u64 = 0;
-    let mut zeros_mask: u64 = 0xFFFFFFFFFFFFFFFF;
-    let mut memory = Vec::<u64>::new();
-
-    lines_from_file("input/day_14.txt", |line| {
-        if line.starts_with("mask = ") {
-            ones_mask = 0;
-            zeros_mask = 0xFFFFFFFFFFFFFFFF;
-            for i in 0..36 {
-                match line.chars().nth(7 + 36 - i - 1).unwrap() {
-                    'X' => {},
-                    '1' => ones_mask |= 1 << i,
-                    '0' => zeros_mask &= !(1 << i),
-                    _ => panic!()
-                }
-            }
-        } else if line.starts_with("mem[") {
-            let address: usize;
-            let value: u64;
-            scan!(line.bytes() => "mem[{}] = {}", address, value);
-            if address >= memory.len() {
-                memory.resize(address + 1, 0);
-            }
-            memory[address] = (value | ones_mask) & zeros_mask;
-        }
-    });
-
-    let mut sum = 0;
-    for val in memory {
-        sum += val;
-    }
-    println!("Part one: {}", sum);
+enum Command {
+    SetMask { ones: u64, zeros: u64 },
+    SetMemory { address: u64, value: u64 }
 }
 
-fn part_two() {
-    let mut ones_mask: usize = 0;
-    let mut floating_bits = Vec::<usize>::new();
-    let mut memory = std::collections::hash_map::HashMap::<usize, u64>::new();
+type Memory = std::collections::HashMap::<u64, u64>;
 
-    lines_from_file("input/day_14.txt", |line| {
-        if line.starts_with("mask = ") {
-            ones_mask = 0;
-            floating_bits.clear();
-            for i in 0..36 {
-                match line.chars().nth(7 + 36 - i - 1).unwrap() {
-                    '0' => {},
-                    '1' => ones_mask |= 1 << i,
-                    'X' => floating_bits.push(i),
-                    _ => panic!()
-                }
-            }
-        } else if line.starts_with("mem[") {
-            let mut address: usize;
-            let value: u64;
-            scan!(line.bytes() => "mem[{}] = {}", address, value);
-            address = address | ones_mask;
-            for i in 0..(1 << floating_bits.len()) {
-                for j in 0..floating_bits.len() {
-                    address &= !(1 << floating_bits[j]);
-                    if (i >> j) & 1 == 1 {
-                        address |= 1 << floating_bits[j];
+fn parse_input() -> Vec::<Command> {
+    line_iter_from_file("input/day_14.txt")
+        .map(|line| {
+            let line_bytes = line.as_bytes();
+            if line_bytes.starts_with(b"mask = ") {
+                let mut ones = 0;
+                let mut zeros = 0;
+                for bit in 0..36 {
+                    match line_bytes[line_bytes.len() - 1 - bit] {
+                        b'0' => zeros |= 1 << bit,
+                        b'1' => ones |= 1 << bit,
+                        _ => {}
                     }
                 }
-                memory.insert(address, value);
+                Command::SetMask { ones, zeros }
+            } else {
+                let address: u64;
+                let value: u64;
+                // line.bytes() works but line.as_bytes().iter() doesn't.
+                // Rust is hard
+                scan!(line.bytes() => "mem[{}] = {}", address, value);
+                Command::SetMemory { address, value }
+            }
+        })
+        .collect()
+}
+
+fn memory_sum(memory: Memory) -> u64 {
+    memory.iter().map(|(_, value)| value).sum()
+}
+
+fn part_one(commands: &Vec::<Command>) -> u64 {
+    let mut ones_mask: u64 = 0;
+    let mut zeros_mask: u64 = 0xFFFFFFFFFFFFFFFF;
+    let mut memory = Memory::new();
+
+    for command in commands {
+        match command {
+            Command::SetMask { ones, zeros } => {
+                ones_mask = *ones;
+                zeros_mask = !*zeros;
+            },
+            Command::SetMemory { address, value } => {
+                memory.insert(*address, (value | ones_mask) & zeros_mask);
             }
         }
-    });
-
-    let mut sum = 0;
-    for (addr, val) in memory {
-        sum += val;
     }
-    println!("Part two: {}", sum);
+
+    memory_sum(memory)
+}
+
+fn part_two(commands: &Vec::<Command>) -> u64 {
+    let mut ones_mask: u64 = 0;
+    let mut floating_bits = Vec::<u8>::new();
+    let mut memory = Memory::new();
+
+    for command in commands {
+        match command {
+            Command::SetMask { ones, zeros } => {
+                ones_mask = *ones;
+                let xes = !(ones | zeros);
+                floating_bits.clear();
+                for bit in 0..36 {
+                    if (xes >> bit) & 1 == 1 {
+                        floating_bits.push(bit);
+                    }
+                }
+            },
+            Command::SetMemory { address, value } => {
+                let mut address = *address;
+                address |= ones_mask;
+                for power_set_elem in 0..(1 << floating_bits.len()) {
+                    for bit in 0..floating_bits.len() {
+                        address &= !(1 << floating_bits[bit]);
+                        address |= ((power_set_elem >> bit) & 1) << floating_bits[bit];
+                    }
+                    memory.insert(address, *value);
+                }
+            }
+        }
+    }
+
+    memory_sum(memory)
 }
 
 fn main() {
-    part_one();
-    part_two();
+    let commands = parse_input();
+    println!("Part one: {}", part_one(&commands));
+    println!("Part two: {}", part_two(&commands));
 }
