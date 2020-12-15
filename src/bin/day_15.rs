567cbe2683dbffc359ba943e25aadf46580b5cf8
Kerndog73 <kerndog73@gmail.com>
2020-12-15 18:03:27 +1030

    Simplify day 15

--- src/bin/day_15.rs
+++ src/bin/day_15.rs
@@ -9,25 +9,11 @@ fn find_spoken_number(start: &[u32], last_turn: u32) -> u32 {
     let mut last = start[start.len() - 1];
 
     for t in (start.len() as u32)..last_turn {
-        match numbers.entry(last) {
-            Entry::Occupied(entry) => {
-                let pair = entry.get();
-                last = pair.1 - pair.0;
-                match numbers.entry(last) {
-                    Entry::Occupied(mut entry) => {
-                        let pair = entry.get_mut();
-                        pair.0 = pair.1;
-                        pair.1 = t;
-                    },
-                    Entry::Vacant(entry) => {
-                        entry.insert((t, t));
-                    }
-                }
-            },
-            Entry::Vacant(entry) => {
-                entry.insert((t, t));
-            }
-        }
+        let pair = numbers[&last];
+        last = pair.1 - pair.0;
+        let pair = numbers.entry(last).or_insert((t, t));
+        pair.0 = pair.1;
+        pair.1 = t;
     }
 
     last
