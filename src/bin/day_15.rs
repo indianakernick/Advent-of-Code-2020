177b3191dd2991b6964c96ae030a1842b9bfd7f3
Kerndog73 <kerndog73@gmail.com>
2020-12-15 17:28:30 +1030

    Don't need to add 1

--- src/bin/day_15.rs
+++ src/bin/day_15.rs
@@ -3,12 +3,12 @@ use std::collections::hash_map::{HashMap, Entry};
 fn find_spoken_number(start: &[u32], last_turn: u32) -> u32 {
     let mut numbers = HashMap::<u32, (u32, u32)>::new();
     for i in 0..start.len() {
-        let turn = i as u32 + 1;
+        let turn = i as u32;
         numbers.insert(start[i], (turn, turn));
     }
     let mut last = start[start.len() - 1];
 
-    for t in (start.len() as u32 + 1)..(last_turn + 1) {
+    for t in (start.len() as u32)..last_turn {
         match numbers.entry(last) {
             Entry::Occupied(entry) => {
                 let pair = entry.get();
