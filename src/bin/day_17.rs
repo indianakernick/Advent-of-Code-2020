f3c6c6f283e357302ff8a8a6ab68a1194dca3f85
Kerndog73 <kerndog73@gmail.com>
2020-12-17 19:24:10 +1030

    Further cleanup of day 17

--- src/bin/day_17.rs
+++ src/bin/day_17.rs
@@ -1,18 +1,6 @@
 use adventofcode2020::*;
 
-/*
-.###.#.#
-####.#.#
-#.....#.
-####....
-#...##.#
-########
-..#####.
-######.#
-*/
-
 type Pos = (i32, i32, i32, i32);
-
 type Cells = std::collections::HashSet<Pos>;
 
 struct Range {
@@ -33,96 +21,43 @@ impl Range {
     fn iter(&self) -> std::ops::RangeInclusive<i32> {
         (self.min - 1)..=(self.max + 1)
     }
+
+    fn add_to_range(&mut self, value: i32) {
+        self.min = self.min.min(value);
+        self.max = self.max.max(value);
+    }
 }
 
 type Range3D = (Range, Range, Range);
 type Range4D = (Range, Range, Range, Range);
 
 fn add_to_range_3d(range: &mut Range3D, pos: Pos) {
-    range.0 = Range {
-        min: range.0.min.min(pos.0),
-        max: range.0.max.max(pos.0)
-    };
-    range.1 = Range {
-        min: range.1.min.min(pos.1),
-        max: range.1.max.max(pos.1)
-    };
-    range.2 = Range {
-        min: range.2.min.min(pos.2),
-        max: range.2.max.max(pos.2)
-    };
+    range.0.add_to_range(pos.0);
+    range.1.add_to_range(pos.1);
+    range.2.add_to_range(pos.2);
 }
 
 fn add_to_range_4d(range: &mut Range4D, pos: Pos) {
-    range.0 = Range {
-        min: range.0.min.min(pos.0),
-        max: range.0.max.max(pos.0)
-    };
-    range.1 = Range {
-        min: range.1.min.min(pos.1),
-        max: range.1.max.max(pos.1)
-    };
-    range.2 = Range {
-        min: range.2.min.min(pos.2),
-        max: range.2.max.max(pos.2)
-    };
-    range.3 = Range {
-        min: range.3.min.min(pos.3),
-        max: range.3.max.max(pos.3)
-    };
+    range.0.add_to_range(pos.0);
+    range.1.add_to_range(pos.1);
+    range.2.add_to_range(pos.2);
+    range.3.add_to_range(pos.3);
 }
 
 fn parse_input() -> Cells {
     let mut cells = Cells::new();
+    let mut y = 0;
 
-    cells.insert((1, 0, 0, 0));
-    cells.insert((2, 0, 0, 0));
-    cells.insert((3, 0, 0, 0));
-    cells.insert((5, 0, 0, 0));
-    cells.insert((7, 0, 0, 0));
-
-    cells.insert((0, 1, 0, 0));
-    cells.insert((1, 1, 0, 0));
-    cells.insert((2, 1, 0, 0));
-    cells.insert((3, 1, 0, 0));
-    cells.insert((5, 1, 0, 0));
-    cells.insert((7, 1, 0, 0));
-
-    cells.insert((0, 2, 0, 0));
-    cells.insert((6, 2, 0, 0));
-
-    cells.insert((0, 3, 0, 0));
-    cells.insert((1, 3, 0, 0));
-    cells.insert((2, 3, 0, 0));
-    cells.insert((3, 3, 0, 0));
-
-    cells.insert((0, 4, 0, 0));
-    cells.insert((4, 4, 0, 0));
-    cells.insert((5, 4, 0, 0));
-    cells.insert((7, 4, 0, 0));
-
-    cells.insert((0, 5, 0, 0));
-    cells.insert((1, 5, 0, 0));
-    cells.insert((2, 5, 0, 0));
-    cells.insert((3, 5, 0, 0));
-    cells.insert((4, 5, 0, 0));
-    cells.insert((5, 5, 0, 0));
-    cells.insert((6, 5, 0, 0));
-    cells.insert((7, 5, 0, 0));
-
-    cells.insert((2, 6, 0, 0));
-    cells.insert((3, 6, 0, 0));
-    cells.insert((4, 6, 0, 0));
-    cells.insert((5, 6, 0, 0));
-    cells.insert((6, 6, 0, 0));
-
-    cells.insert((0, 7, 0, 0));
-    cells.insert((1, 7, 0, 0));
-    cells.insert((2, 7, 0, 0));
-    cells.insert((3, 7, 0, 0));
-    cells.insert((4, 7, 0, 0));
-    cells.insert((5, 7, 0, 0));
-    cells.insert((7, 7, 0, 0));
+    lines_from_file("input/day_17.txt", |line| {
+        let mut x = 0;
+        for ch in line.chars() {
+            if ch == '#' {
+                cells.insert((x, y, 0, 0));
+            }
+            x += 1;
+        }
+        y += 1;
+    });
 
     cells
 }
@@ -207,17 +142,11 @@ fn part_one(mut cells: Cells) -> usize {
     let mut alt_cells = Cells::new();
     let mut range = (Range::new(0, 7), Range::new(0, 7), Range::new(0, 0));
 
-    range = simulate_3d(range, &mut alt_cells, &cells); // 1
-    cells.clear();
-    range = simulate_3d(range, &mut cells, &alt_cells); // 2
-    alt_cells.clear();
-    range = simulate_3d(range, &mut alt_cells, &cells); // 3
-    cells.clear();
-    range = simulate_3d(range, &mut cells, &alt_cells); // 4
-    alt_cells.clear();
-    range = simulate_3d(range, &mut alt_cells, &cells); // 5
-    cells.clear();
-    simulate_3d(range, &mut cells, &alt_cells); // 6
+    for _ in 0..6 {
+        alt_cells.clear();
+        range = simulate_3d(range, &mut alt_cells, &cells);
+        std::mem::swap(&mut alt_cells, &mut cells);
+    }
 
     cells.len()
 }
@@ -226,17 +155,11 @@ fn part_two(mut cells: Cells) -> usize {
     let mut alt_cells = Cells::new();
     let mut range = (Range::new(0, 7), Range::new(0, 7), Range::new(0, 0), Range::new(0, 0));
 
-    range = simulate_4d(range, &mut alt_cells, &cells); // 1
-    cells.clear();
-    range = simulate_4d(range, &mut cells, &alt_cells); // 2
-    alt_cells.clear();
-    range = simulate_4d(range, &mut alt_cells, &cells); // 3
-    cells.clear();
-    range = simulate_4d(range, &mut cells, &alt_cells); // 4
-    alt_cells.clear();
-    range = simulate_4d(range, &mut alt_cells, &cells); // 5
-    cells.clear();
-    simulate_4d(range, &mut cells, &alt_cells); // 6
+    for _ in 0..6 {
+        alt_cells.clear();
+        range = simulate_4d(range, &mut alt_cells, &cells);
+        std::mem::swap(&mut alt_cells, &mut cells);
+    }
 
     cells.len()
 }
