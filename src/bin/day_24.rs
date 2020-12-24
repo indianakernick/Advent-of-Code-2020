c8d3be9b1e2175aadf911e39b918afd9188353a1
Kerndog73 <kerndog73@gmail.com>
2020-12-24 16:36:50 +1030

    Clean up day 24

--- src/bin/day_24.rs
+++ src/bin/day_24.rs
@@ -1,52 +1,58 @@
 use adventofcode2020::*;
 use std::collections::HashSet;
 
-fn main() {
-    let mut every_tile = HashSet::<(i32, i32, i32)>::new();
-    let mut set = HashSet::<(i32, i32, i32)>::new();
+fn parse_input() -> HashSet::<(i32, i32, i32)> {
+    let mut tiles = HashSet::new();
 
     lines_from_file("input/day_24.txt", |line| {
-        let mut position = (0, 0, 0);
+        let mut tile = (0, 0, 0);
         let mut char_iter = line.chars();
+
         loop {
             let ch = match char_iter.next() {
                 Some(ch) => ch,
                 None => break
             };
+
+            // https://www.redblobgames.com/grids/hexagons/
             match ch {
-                'e' => { position.0 += 1; position.1 -= 1; }
-                'w' => { position.0 -= 1; position.1 += 1; }
+                'e' => { tile.0 += 1; tile.1 -= 1; }
+                'w' => { tile.0 -= 1; tile.1 += 1; }
                 'n' => match char_iter.next().unwrap() {
-                    'e' => { position.0 += 1; position.2 -= 1; }
-                    'w' => { position.1 += 1; position.2 -= 1;}
+                    'e' => { tile.0 += 1; tile.2 -= 1; }
+                    'w' => { tile.1 += 1; tile.2 -= 1; }
                     _ => panic!()
                 },
                 's' => match char_iter.next().unwrap() {
-                    'e' => { position.1 -= 1; position.2 += 1; }
-                    'w' => { position.0 -= 1; position.2 += 1; }
+                    'e' => { tile.1 -= 1; tile.2 += 1; }
+                    'w' => { tile.0 -= 1; tile.2 += 1; }
                     _ => panic!()
                 },
                 _ => panic!()
             }
         }
-        every_tile.insert(position);
-        if set.contains(&position) {
-            set.remove(&position);
-        } else {
-            set.insert(position);
+
+        if !tiles.remove(&tile) {
+            tiles.insert(tile);
         }
     });
 
-    println!("Part one: {}", set.len());
+    tiles
+}
+
+fn simulate(tiles: &mut HashSet<(i32, i32, i32)>) {
+    let mut next_tiles = HashSet::<(i32, i32, i32)>::new();
 
     for _ in 0..100 {
+        next_tiles.clear();
+
         let mut min_x = i32::MAX;
         let mut max_x = i32::MIN;
         let mut min_y = i32::MAX;
         let mut max_y = i32::MIN;
         let mut min_z = i32::MAX;
         let mut max_z = i32::MIN;
-        for tile in set.iter() {
+        for tile in tiles.iter() {
             min_x = min_x.min(tile.0);
             max_x = max_x.max(tile.0);
             min_y = min_y.min(tile.1);
@@ -55,43 +61,37 @@ fn main() {
             max_z = max_z.max(tile.2);
         }
 
-        let mut next_set = HashSet::<(i32, i32, i32)>::new();
-
         for x in (min_x - 1)..=(max_x + 1) {
             for y in (min_y - 1)..=(max_y + 1) {
                 for z in (min_z - 1)..=(max_z + 1) {
                     if x + y + z != 0 {
                         continue;
                     }
-                    let tile = &(x, y, z);
                     let neighbors = [
-                        (tile.0 + 1, tile.1 - 1, tile.2),
-                        (tile.0 - 1, tile.1 + 1, tile.2),
-                        (tile.0 + 1, tile.1,     tile.2 - 1),
-                        (tile.0,     tile.1 + 1, tile.2 - 1),
-                        (tile.0,     tile.1 - 1, tile.2 + 1),
-                        (tile.0 - 1, tile.1,     tile.2 + 1),
+                        (x + 1, y - 1, z    ),
+                        (x - 1, y + 1, z    ),
+                        (x + 1, y,     z - 1),
+                        (x,     y + 1, z - 1),
+                        (x,     y - 1, z + 1),
+                        (x - 1, y,     z + 1),
                     ];
-                    let mut black_count = 0;
-                    for neighbor in neighbors.iter() {
-                        if set.contains(neighbor) {
-                            black_count += 1;
-                        }
-                    }
-                    if set.contains(tile) {
-                        if black_count == 1 || black_count == 2 {
-                            next_set.insert(*tile);
-                        }
-                    } else {
-                        if black_count == 2 {
-                            next_set.insert(*tile);
-                        }
+                    let black_count = neighbors.iter()
+                        .filter(|neighbor| tiles.contains(neighbor))
+                        .count();
+                    if black_count == 2 || (black_count == 1 && tiles.contains(&(x, y, z))) {
+                        next_tiles.insert((x, y, z));
                     }
                 }
             }
         }
 
-        set = next_set;
+        std::mem::swap(tiles, &mut next_tiles);
     }
-    println!("Part two: {}", set.len());
+}
+
+fn main() {
+    let mut tiles = parse_input();
+    println!("Part one: {}", tiles.len());
+    simulate(&mut tiles);
+    println!("Part two: {}", tiles.len());
 }
