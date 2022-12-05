9a7c89507cfa4a32165db001ceecf0b107fdb1d3
Indiana Kernick <indiana.kernick@gmail.com>
2022-12-05 16:06:30 +1030

    Clean up day 5

--- 2022/src/bin/day_05.rs
+++ 2022/src/bin/day_05.rs
@@ -1,100 +1,58 @@
 use advent_of_code_2022 as util;
 use text_io::read;
 
+fn stack_tops(stacks: &[Vec<char>]) -> String {
+    let mut s = String::new();
+
+    for stack in stacks.iter() {
+        if let Some(top) = stack.last() {
+            s.push(*top);
+        }
+    }
+
+    s
+}
+
 fn main() {
-    /*
-    [F]         [L]     [M]
-    [T]     [H] [V] [G] [V]
-    [N]     [T] [D] [R] [N]     [D]
-    [Z]     [B] [C] [P] [B] [R] [Z]
-    [M]     [J] [N] [M] [F] [M] [V] [H]
-    [G] [J] [L] [J] [S] [C] [G] [M] [F]
-    [H] [W] [V] [P] [W] [H] [H] [N] [N]
-    [J] [V] [G] [B] [F] [G] [D] [H] [G]
-     1   2   3   4   5   6   7   8   9
-    */
+    let mut stacks_1 = vec![
+        vec!['J', 'H', 'G', 'M', 'Z', 'N', 'T', 'F'],
+        vec!['V', 'W', 'J'],
+        vec!['G', 'V', 'L', 'J', 'B', 'T', 'H'],
+        vec!['B', 'P', 'J', 'N', 'C', 'D', 'V', 'L'],
+        vec!['F', 'W', 'S', 'M', 'P', 'R', 'G'],
+        vec!['G', 'H', 'C', 'F', 'B', 'N', 'V', 'M'],
+        vec!['D', 'H', 'G', 'M', 'R'],
+        vec!['H', 'N', 'M', 'V', 'Z', 'D'],
+        vec!['G', 'N', 'F', 'H'],
+    ];
+    let mut stacks_2 = stacks_1.clone();
+    let mut skip = true;
 
-    {
-        let mut crates = vec![
-            vec!['J', 'H', 'G', 'M', 'Z', 'N', 'T', 'F'],
-            vec!['V', 'W', 'J'],
-            vec!['G', 'V', 'L', 'J', 'B', 'T', 'H'],
-            vec!['B', 'P', 'J', 'N', 'C', 'D', 'V', 'L'],
-            vec!['F', 'W', 'S', 'M', 'P', 'R', 'G'],
-            vec!['G', 'H', 'C', 'F', 'B', 'N', 'V', 'M'],
-            vec!['D', 'H', 'G', 'M', 'R'],
-            vec!['H', 'N', 'M', 'V', 'Z', 'D'],
-            vec!['G', 'N', 'F', 'H'],
-        ];
-
-        let mut index = 0;
-
-        util::each_line("input/day_05.txt", |line| {
-            if index <= 9 {
-                index += 1;
-                return;
-            }
-
-            let mut line_iter = line.bytes();
-
-            let count: usize = read!("move {} from ", line_iter);
-            let from: usize = read!("{} to ", line_iter);
-            let to: usize = read!("{}", line_iter);
-            let from = from - 1;
-            let to = to - 1;
-
-            for _ in 0..count {
-                let top = crates[from].pop().unwrap();
-                crates[to].push(top);
-            }
-        });
-
-        print!("Part 1: ");
-        for crate_ in crates.iter() {
-            print!("{}", crate_.last().unwrap());
+    util::each_line("input/day_05.txt", |line| {
+        if skip {
+            skip = !line.is_empty();
+            return;
         }
-        println!();
-    }
 
-    {
-        let mut crates = vec![
-            vec!['J', 'H', 'G', 'M', 'Z', 'N', 'T', 'F'],
-            vec!['V', 'W', 'J'],
-            vec!['G', 'V', 'L', 'J', 'B', 'T', 'H'],
-            vec!['B', 'P', 'J', 'N', 'C', 'D', 'V', 'L'],
-            vec!['F', 'W', 'S', 'M', 'P', 'R', 'G'],
-            vec!['G', 'H', 'C', 'F', 'B', 'N', 'V', 'M'],
-            vec!['D', 'H', 'G', 'M', 'R'],
-            vec!['H', 'N', 'M', 'V', 'Z', 'D'],
-            vec!['G', 'N', 'F', 'H'],
-        ];
+        let mut line_iter = line.bytes();
 
-        let mut index = 0;
+        let count: usize = read!("move {} from ", line_iter);
+        let from: usize = read!("{} to ", line_iter);
+        let to: usize = read!("{}", line_iter);
+        let from = from - 1;
+        let to = to - 1;
 
-        util::each_line("input/day_05.txt", |line| {
-            if index <= 9 {
-                index += 1;
-                return;
-            }
-
-            let mut line_iter = line.bytes();
-
-            let count: usize = read!("move {} from ", line_iter);
-            let from: usize = read!("{} to ", line_iter);
-            let to: usize = read!("{}", line_iter);
-            let from = from - 1;
-            let to = to - 1;
-
-            let top = Vec::from(&crates[from][crates[from].len() - count..]);
-            crates[to].extend(top.iter());
-            let new_size = crates[from].len() - count;
-            crates[from].resize(new_size, '0');
-        });
-
-        print!("Part 2: ");
-        for crate_ in crates.iter() {
-            print!("{}", crate_.last().unwrap());
+        for _ in 0..count {
+            let top = stacks_1[from].pop().unwrap();
+            stacks_1[to].push(top);
         }
-        println!();
-    }
+
+        let new_size = stacks_2[from].len() - count;
+        let top = Vec::from(&stacks_2[from][new_size..]);
+        stacks_2[to].extend(top.iter());
+        stacks_2[from].truncate(new_size);
+    });
+
+    println!("Part 1: {}", stack_tops(&stacks_1));
+    println!("Part 2: {}", stack_tops(&stacks_2));
 }
