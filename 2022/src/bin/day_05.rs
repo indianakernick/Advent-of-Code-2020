cca49c057a56f9de9fbf19cb6f171c4c7456095f
Indiana Kernick <indiana.kernick@gmail.com>
2022-12-05 15:47:29 +1030

    Day 5, part 2

--- 2022/src/bin/day_05.rs
+++ 2022/src/bin/day_05.rs
@@ -14,43 +14,87 @@ fn main() {
      1   2   3   4   5   6   7   8   9
     */
 
-    let mut crates = vec![
-        vec!['J', 'H', 'G', 'M', 'Z', 'N', 'T', 'F'],
-        vec!['V', 'W', 'J'],
-        vec!['G', 'V', 'L', 'J', 'B', 'T', 'H'],
-        vec!['B', 'P', 'J', 'N', 'C', 'D', 'V', 'L'],
-        vec!['F', 'W', 'S', 'M', 'P', 'R', 'G'],
-        vec!['G', 'H', 'C', 'F', 'B', 'N', 'V', 'M'],
-        vec!['D', 'H', 'G', 'M', 'R'],
-        vec!['H', 'N', 'M', 'V', 'Z', 'D'],
-        vec!['G', 'N', 'F', 'H'],
-    ];
+    {
+        let mut crates = vec![
+            vec!['J', 'H', 'G', 'M', 'Z', 'N', 'T', 'F'],
+            vec!['V', 'W', 'J'],
+            vec!['G', 'V', 'L', 'J', 'B', 'T', 'H'],
+            vec!['B', 'P', 'J', 'N', 'C', 'D', 'V', 'L'],
+            vec!['F', 'W', 'S', 'M', 'P', 'R', 'G'],
+            vec!['G', 'H', 'C', 'F', 'B', 'N', 'V', 'M'],
+            vec!['D', 'H', 'G', 'M', 'R'],
+            vec!['H', 'N', 'M', 'V', 'Z', 'D'],
+            vec!['G', 'N', 'F', 'H'],
+        ];
 
-    let mut index = 0;
+        let mut index = 0;
 
-    util::each_line("input/day_05.txt", |line| {
-        if index <= 9 {
-            index += 1;
-            return;
+        util::each_line("input/day_05.txt", |line| {
+            if index <= 9 {
+                index += 1;
+                return;
+            }
+
+            let mut line_iter = line.bytes();
+
+            let count: usize = read!("move {} from ", line_iter);
+            let from: usize = read!("{} to ", line_iter);
+            let to: usize = read!("{}", line_iter);
+            let from = from - 1;
+            let to = to - 1;
+
+            for _ in 0..count {
+                let top = crates[from].pop().unwrap();
+                crates[to].push(top);
+            }
+        });
+
+        print!("Part 1: ");
+        for crate_ in crates.iter() {
+            print!("{}", crate_.last().unwrap());
         }
-
-        let mut line_iter = line.bytes();
-
-        let count: usize = read!("move {} from ", line_iter);
-        let from: usize = read!("{} to ", line_iter);
-        let to: usize = read!("{}", line_iter);
-        let from = from - 1;
-        let to = to - 1;
-
-        for _ in 0..count {
-            let top = crates[from].pop().unwrap();
-            crates[to].push(top);
-        }
-    });
-
-    print!("Part 1: ");
-    for crate_ in crates.iter() {
-        print!("{}", crate_.last().unwrap());
+        println!();
     }
-    println!();
+
+    {
+        let mut crates = vec![
+            vec!['J', 'H', 'G', 'M', 'Z', 'N', 'T', 'F'],
+            vec!['V', 'W', 'J'],
+            vec!['G', 'V', 'L', 'J', 'B', 'T', 'H'],
+            vec!['B', 'P', 'J', 'N', 'C', 'D', 'V', 'L'],
+            vec!['F', 'W', 'S', 'M', 'P', 'R', 'G'],
+            vec!['G', 'H', 'C', 'F', 'B', 'N', 'V', 'M'],
+            vec!['D', 'H', 'G', 'M', 'R'],
+            vec!['H', 'N', 'M', 'V', 'Z', 'D'],
+            vec!['G', 'N', 'F', 'H'],
+        ];
+
+        let mut index = 0;
+
+        util::each_line("input/day_05.txt", |line| {
+            if index <= 9 {
+                index += 1;
+                return;
+            }
+
+            let mut line_iter = line.bytes();
+
+            let count: usize = read!("move {} from ", line_iter);
+            let from: usize = read!("{} to ", line_iter);
+            let to: usize = read!("{}", line_iter);
+            let from = from - 1;
+            let to = to - 1;
+
+            let top = Vec::from(&crates[from][crates[from].len() - count..]);
+            crates[to].extend(top.iter());
+            let new_size = crates[from].len() - count;
+            crates[from].resize(new_size, '0');
+        });
+
+        print!("Part 2: ");
+        for crate_ in crates.iter() {
+            print!("{}", crate_.last().unwrap());
+        }
+        println!();
+    }
 }
