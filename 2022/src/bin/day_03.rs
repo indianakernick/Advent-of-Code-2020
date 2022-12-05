83cc25e0c44f1008a3b0660f46d05e5d6d5ddc6b
Indiana Kernick <indiana.kernick@gmail.com>
2022-12-05 11:55:49 +1030

    Exactly one item type

--- 2022/src/bin/day_03.rs
+++ 2022/src/bin/day_03.rs
@@ -2,22 +2,19 @@ use advent_of_code_2022 as util;
 
 fn main() {
     let mut sum = 0u64;
-    let mut found = Vec::new();
 
     util::each_line("input/day_03.txt", |line| {
         let first = line[..line.len() / 2].as_bytes();
         let second = line[line.len() / 2..].as_bytes();
 
-        found.clear();
-
         for item in first.iter() {
-            if !found.contains(item) && second.contains(item) {
-                found.push(*item);
+            if second.contains(item) {
                 if item.is_ascii_lowercase() {
                     sum += (item - b'a' + 1) as u64;
-                } else if item.is_ascii_uppercase() {
+                } else {
                     sum += (item - b'A' + 27) as u64;
                 }
+                break;
             }
         }
     });
