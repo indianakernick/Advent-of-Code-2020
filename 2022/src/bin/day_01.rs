28fe9b36d9cc88fa2e6fce0be07516259d0b98ff
Indiana Kernick <indiana.kernick@gmail.com>
2022-12-05 10:50:09 +1030

    Day 1, part 2

--- 2022/src/bin/day_01.rs
+++ 2022/src/bin/day_01.rs
@@ -1,17 +1,29 @@
 use advent_of_code_2022 as util;
 
 fn main() {
-    let mut max = 0u64;
+    const PREFIX_COUNT: usize = 3;
+
+    let mut max = Vec::with_capacity(PREFIX_COUNT);
     let mut curr = 0u64;
 
     util::each_line("input/day_01.txt", |line| {
         if line.len() == 0 {
-            max = max.max(curr);
+            if max.len() == 0 {
+                max.push(curr);
+            } else if max[max.len() - 1] < curr {
+                if max.len() == PREFIX_COUNT {
+                    max.pop();
+                }
+                max.push(curr);
+                max.sort_unstable_by(|a, b| b.cmp(a));
+            }
+
             curr = 0;
         } else {
             curr += u64::from_str_radix(line, 10).unwrap();
         }
     });
 
-    println!("Part 1: {}", max);
+    println!("Part 1: {}", max[0]);
+    println!("Part 2: {}", max.iter().sum::<u64>());
 }
