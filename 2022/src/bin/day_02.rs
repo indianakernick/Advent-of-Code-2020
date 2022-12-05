ccb5d1acc05851c67c494309c2efadc9f05ce120
Indiana Kernick <indiana.kernick@gmail.com>
2022-12-05 11:29:43 +1030

    Day 2, part 2

--- 2022/src/bin/day_02.rs
+++ 2022/src/bin/day_02.rs
@@ -1,20 +1,40 @@
 use advent_of_code_2022 as util;
 
 fn main() {
-    let mut score = 0u64;
+    const LOSING_MOVE: [u8; 3] = [
+        2, // Rock defeats Scissors
+        0, // Paper defeats Rock
+        1, // Scissors defeats Paper
+    ];
+
+    let mut score_1 = 0u64;
+    let mut score_2 = 0u64;
 
     util::each_line("input/day_02.txt", |line| {
         let opponent = line.as_bytes()[0] - b'A';
         let me = line.as_bytes()[2] - b'X';
 
-        score += me as u64 + 1;
+        score_1 += me as u64 + 1;
 
         if opponent == me {
-            score += 3;
-        } else if (me == 0 && opponent == 2) || (me == 2 && opponent == 1) || (me == 1 && opponent == 0) {
-            score += 6;
+            score_1 += 3;
+        } else if LOSING_MOVE[me as usize] == opponent {
+            score_1 += 6;
+        }
+
+        let outcome = me;
+
+        score_2 += outcome as u64 * 3;
+
+        if outcome == 0 {
+            score_2 += LOSING_MOVE[opponent as usize] as u64 + 1;
+        } else if outcome == 1 {
+            score_2 += opponent as u64 + 1;
+        } else if outcome == 2 {
+            score_2 += LOSING_MOVE[LOSING_MOVE[opponent as usize] as usize] as u64 + 1;
         }
     });
 
-    println!("Part 1: {}", score);
+    println!("Part 1: {}", score_1);
+    println!("Part 2: {}", score_2);
 }
