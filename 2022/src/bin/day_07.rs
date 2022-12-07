1bfbdb4c11925a85ee780afb7e1b681a366fccdf
Indiana Kernick <indiana.kernick@gmail.com>
2022-12-07 21:03:05 +1030

    Use a flat HashMap for the whole tree

--- 2022/src/bin/day_07.rs
+++ 2022/src/bin/day_07.rs
@@ -1,114 +1,34 @@
-use std::{ptr::NonNull, collections::HashMap};
+use std::collections::HashMap;
 use advent_of_code_2022 as util;
 
-struct Directory {
-    files: HashMap<String, File>,
-    size: Option<usize>,
-}
-
-impl Directory {
-    fn new() -> Self {
-        Self { files: HashMap::new(), size: None }
-    }
-
-    fn add_directory(&mut self, name: &str) {
-        self.files.entry(name.to_owned())
-            .or_insert(File::Directory(Directory::new()));
-    }
-
-    fn add_regular(&mut self, name: &str, size: usize) {
-        self.files.entry(name.to_owned())
-            .or_insert(File::Regular(Regular { size }));
-    }
-}
-
-struct Regular {
+#[derive(Clone, Copy)]
+struct File {
+    dir: bool,
     size: usize,
 }
 
-enum File {
-    Directory(Directory),
-    Regular(Regular),
-}
-
-fn get_dir_size(dir: &mut Directory, small_dir_total: &mut usize) -> usize {
-    if let Some(size) = dir.size {
-        if size <= 100000 {
-            *small_dir_total += size;
-        }
-        return size;
-    }
-
-    let mut size = 0;
-
-    for (_, file) in dir.files.iter_mut() {
-        match file {
-            File::Directory(dir) => {
-                size += get_dir_size(dir, small_dir_total);
-            }
-            File::Regular(reg) => {
-                size += reg.size;
-            }
-        }
-    }
-
-    if size <= 100000 {
-        *small_dir_total += size;
-    }
-
-    dir.size = Some(size);
-    size
-}
-
-fn find_smallest_dir_larger_than(dir: &Directory, minimum: usize, mut current: usize) -> usize {
-    let dir_size = dir.size.unwrap();
-    if dir_size >= minimum && dir_size < current {
-        current = dir_size;
-    }
-
-    for (_, file) in dir.files.iter() {
-        match file {
-            File::Directory(dir) => {
-                current = find_smallest_dir_larger_than(&dir, minimum, current);
-            }
-            File::Regular(_) => continue,
-        }
-    }
-
-    current
-}
-
-fn resolve_current(dir: &mut Directory, current_path: &[String]) -> NonNull<Directory> {
-    if current_path.len() == 0 {
-        NonNull::from(dir)
-    } else {
-        match dir.files.get_mut(&current_path[0]) {
-            Some(File::Directory(next_dir)) => {
-                resolve_current(next_dir, &current_path[1..])
-            }
-            Some(File::Regular(_)) => {
-                panic!("Cannot enter regular file '{}'", &current_path[0]);
-            }
-            None => panic!("Trying to enter a directory that you don't know exists")
-        }
-    }
+fn path_join(parent: &str, child: &str) -> String {
+    let mut path = String::with_capacity(parent.len() + 1 + child.len());
+    path.push_str(parent);
+    path.push('/');
+    path.push_str(child);
+    path
 }
 
 fn main() {
-    let mut root = Directory::new();
-    let mut current_path = Vec::<String>::new();
-    let mut current_dir = NonNull::from(&mut root);
+    let mut current = "/".to_owned();
+    let mut tree = HashMap::<String, File>::new();
 
     util::each_line("input/day_07.txt", |line| {
-        if line.starts_with("$ cd ") {
-            if line.ends_with("/") {
-                current_path.clear();
-            } else if line.ends_with("..") {
-                current_path.pop().unwrap();
+        if let Some(path) = line.strip_prefix("$ cd ") {
+            if path == "/" {
+                current.clear();
+            } else if path == ".." {
+                current.truncate(current.rfind('/').unwrap());
             } else {
-                current_path.push(line["$ cd ".len()..].to_owned());
+                current.push('/');
+                current.push_str(path);
             }
-            current_dir = resolve_current(&mut root, &current_path);
             return;
         }
 
@@ -116,25 +36,47 @@ fn main() {
             return;
         }
 
-        // Rust was the wrong choice...
-        let current_dir = unsafe { current_dir.as_mut() };
-
-        if line.starts_with("dir ") {
-            current_dir.add_directory(&line["dir ".len()..]);
+        if let Some(name) = line.strip_prefix("dir ") {
+            tree.insert(path_join(&current, name), File { dir: true, size: 0 });
             return;
         }
 
-        let space = line.bytes().position(|c| c == b' ').unwrap();
-        let file_size = line[0..space].parse::<usize>().unwrap();
-        let file_name = &line[space + 1..];
-
-        current_dir.add_regular(file_name, file_size);
+        if let Some((size, name)) = line.split_once(' ') {
+            let size = size.parse().unwrap();
+            tree.insert(path_join(&current, name), File { dir: false, size });
+        }
     });
 
-    let mut small_dir_total = 0;
-    let root_size = get_dir_size(&mut root, &mut small_dir_total);
-    println!("Part 1: {}", small_dir_total);
+    let mut paths = tree.iter()
+        .map(|pair| pair.0.clone())
+        .collect::<Vec<_>>();
+    paths.sort_unstable_by(|a, b| b.cmp(a));
+
+    let mut root_size = 0;
+
+    for path in paths.iter() {
+        if let Some((parent_path, _)) = path.rsplit_once('/') {
+            if let Some(file) = tree.get(path).copied() {
+                if parent_path.is_empty() {
+                    root_size += file.size;
+                } else {
+                    if let Some(parent_file) = tree.get_mut(parent_path) {
+                        parent_file.size += file.size;
+                    }
+                }
+            }
+        }
+    }
+
+    println!("Part 1: {}", tree.iter()
+        .filter(|(_, f)| f.dir && f.size <= 100000)
+        .map(|(_, f)| f.size)
+        .sum::<usize>());
 
     let minimum = 30000000 - (70000000 - root_size);
-    println!("Part 2: {}", find_smallest_dir_larger_than(&root, minimum, usize::MAX));
+
+    println!("Part 2: {}", tree.iter()
+        .filter(|(_, f)| f.dir && f.size >= minimum)
+        .map(|(_, f)| f.size)
+        .min().unwrap());
 }
