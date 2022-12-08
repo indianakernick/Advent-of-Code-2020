d91dea4b0b7ab13836aadbe31cd74e8221b89b4a
Indiana Kernick <indiana.kernick@gmail.com>
2022-12-08 16:41:33 +1030

    Clean up day 8

--- 2022/src/bin/day_08.rs
+++ 2022/src/bin/day_08.rs
@@ -4,98 +4,70 @@ fn main() {
     let mut rows = Vec::new();
 
     util::each_line("input/day_08.txt", |line| {
-        rows.push(line
-            .as_bytes()
-            .iter()
-            .map(|b| (*b, false, 1usize))
-            .collect::<Vec<_>>());
+        rows.push(line.as_bytes().to_vec());
     });
 
-    for y in 0..rows.len() {
-        let mut highest = 0u8;
-        for x in 0..rows[y].len() {
-            if rows[y][x].0 > highest {
-                rows[y][x].1 = true;
-                highest = rows[y][x].0;
-            }
-        }
-
-        let mut highest = 0u8;
-        for x in (0..rows[y].len()).rev() {
-            if rows[y][x].0 > highest {
-                rows[y][x].1 = true;
-                highest = rows[y][x].0;
-            }
-        }
-    }
-
-    for x in 0..rows[0].len() {
-        let mut highest = 0u8;
-        for y in 0..rows.len() {
-            if rows[y][x].0 > highest {
-                rows[y][x].1 = true;
-                highest = rows[y][x].0;
-            }
-        }
-
-        let mut highest = 0u8;
-        for y in (0..rows.len()).rev() {
-            if rows[y][x].0 > highest {
-                rows[y][x].1 = true;
-                highest = rows[y][x].0;
-            }
-        }
-    }
-
-    let mut count = 0;
-    for y in 0..rows.len() {
-        for x in 0..rows[y].len() {
-            if rows[y][x].1 {
-                count += 1;
-            }
-        }
-    }
-    println!("Part 1: {}", count);
+    let mut visible_count = 0;
+    let mut max_score = 0;
 
     for y in 0..rows.len() {
+        let vertical_edge = y == 0 || y == rows.len() - 1;
+
         for x in 0..rows[y].len() {
+            let mut visible = vertical_edge || x == 0 || x == rows[y].len() - 1;
+            let mut score = 1;
+            let height = rows[y][x];
+
             for x1 in (0..x).rev() {
-                if rows[y][x1].0 >= rows[y][x].0 || x1 == 0 {
-                    rows[y][x].2 *= x - x1;
+                if x1 == 0 && rows[y][x1] < height {
+                    visible = true;
+                }
+                if x1 == 0 || rows[y][x1] >= height {
+                    score *= x - x1;
                     break;
                 }
             }
 
             for x1 in x + 1..rows[y].len() {
-                if rows[y][x1].0 >= rows[y][x].0 || x1 == rows[y].len() - 1 {
-                    rows[y][x].2 *= x1 - x;
+                if x1 == rows[y].len() - 1 && rows[y][x1] < height {
+                    visible = true;
+                }
+                if x1 == rows[y].len() - 1 || rows[y][x1] >= height {
+                    score *= x1 - x;
                     break;
                 }
             }
 
             for y1 in (0..y).rev() {
-                if rows[y1][x].0 >= rows[y][x].0 || y1 == 0 {
-                    rows[y][x].2 *= y - y1;
+                if y1 == 0 && rows[y1][x] < height {
+                    visible = true;
+                }
+                if y1 == 0 || rows[y1][x] >= height {
+                    score *= y - y1;
                     break;
                 }
             }
 
             for y1 in y + 1..rows.len() {
-                if rows[y1][x].0 >= rows[y][x].0 || y1 == rows.len() - 1 {
-                    rows[y][x].2 *= y1 - y;
+                if y1 == rows.len() - 1 && rows[y1][x] < height {
+                    visible = true;
+                }
+                if y1 == rows.len() - 1 || rows[y1][x] >= height {
+                    score *= y1 - y;
                     break;
                 }
+            }
+
+            if visible {
+                visible_count += 1;
+            }
+
+            if score > max_score {
+                max_score = score;
             }
         }
     }
 
-    let mut max = 0;
-    for y in 0..rows.len() {
-        for x in 0..rows[y].len() {
-            if rows[y][x].2 > max {
-                max = rows[y][x].2;
-            }
-        }
-    }
-    println!("Part 2: {}", max);
+    println!("Part 1: {}", visible_count);
+    println!("Part 2: {}", max_score);
 }
