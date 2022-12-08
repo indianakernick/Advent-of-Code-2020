e28e5bc5977dc150a5213b61f7fa6e91ad167b17
Indiana Kernick <indiana.kernick@gmail.com>
2022-12-08 16:04:07 +1030

    Day 8, part 2

--- 2022/src/bin/day_08.rs
+++ 2022/src/bin/day_08.rs
@@ -7,7 +7,7 @@ fn main() {
         rows.push(line
             .as_bytes()
             .iter()
-            .map(|b| (*b, false))
+            .map(|b| (*b, false, 1usize))
             .collect::<Vec<_>>());
     });
 
@@ -56,4 +56,46 @@ fn main() {
         }
     }
     println!("Part 1: {}", count);
+
+    for y in 0..rows.len() {
+        for x in 0..rows[y].len() {
+            for x1 in (0..x).rev() {
+                if rows[y][x1].0 >= rows[y][x].0 || x1 == 0 {
+                    rows[y][x].2 *= x - x1;
+                    break;
+                }
+            }
+
+            for x1 in x + 1..rows[y].len() {
+                if rows[y][x1].0 >= rows[y][x].0 || x1 == rows[y].len() - 1 {
+                    rows[y][x].2 *= x1 - x;
+                    break;
+                }
+            }
+
+            for y1 in (0..y).rev() {
+                if rows[y1][x].0 >= rows[y][x].0 || y1 == 0 {
+                    rows[y][x].2 *= y - y1;
+                    break;
+                }
+            }
+
+            for y1 in y + 1..rows.len() {
+                if rows[y1][x].0 >= rows[y][x].0 || y1 == rows.len() - 1 {
+                    rows[y][x].2 *= y1 - y;
+                    break;
+                }
+            }
+        }
+    }
+
+    let mut max = 0;
+    for y in 0..rows.len() {
+        for x in 0..rows[y].len() {
+            if rows[y][x].2 > max {
+                max = rows[y][x].2;
+            }
+        }
+    }
+    println!("Part 2: {}", max);
 }
