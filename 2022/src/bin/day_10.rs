ba36ca8c6b09edad967242b090bc297c3a0864e5
Indiana Kernick <indiana.kernick@gmail.com>
2022-12-10 16:19:24 +1030

    Clean up day 10

--- 2022/src/bin/day_10.rs
+++ 2022/src/bin/day_10.rs
@@ -1,55 +1,35 @@
 use advent_of_code_2022 as util;
 
+fn run_cycle(reg: i32, cycle: &mut i32, strength: &mut i32, display: &mut [u8]) {
+    let x = *cycle % 40;
+    if x == reg || x == reg - 1 || x == reg + 1 {
+        display[*cycle as usize] = b'#';
+    }
+    *cycle += 1;
+    if *cycle >= 20 && (*cycle - 20) % 40 == 0 {
+        *strength += *cycle * reg;
+    }
+}
+
 fn main() {
     let mut reg = 1;
     let mut cycle = 0;
     let mut strength = 0;
-
-    let mut display = Vec::<Vec<u8>>::new();
-    display.resize_with(6, || vec![b'.'; 40]);
+    let mut display = vec![b'.'; 6 * 40];
 
     util::each_line("input/day_10.txt", |line| {
         if line == "noop" {
-            cycle += 1;
-            if cycle >= 20 && (cycle - 20) % 40 == 0 {
-                strength += cycle * reg;
-            }
-            let y = (cycle - 1) / 40;
-            let x = (cycle - 1) % 40;
-            if x == reg || x == reg - 1 || x == reg + 1 {
-                display[y as usize][x as usize] = b'#';
-            }
+            run_cycle(reg, &mut cycle, &mut strength, &mut display);
         } else if let Some(num) = line.strip_prefix("addx ") {
-            let num: i32 = num.parse().unwrap();
-            cycle += 1;
-            if cycle >= 20 && (cycle - 20) % 40 == 0 {
-                strength += cycle * reg;
-            }
-            let y = (cycle - 1) / 40;
-            let x = (cycle - 1) % 40;
-            if x == reg || x == reg - 1 || x == reg + 1 {
-                display[y as usize][x as usize] = b'#';
-            }
-            cycle += 1;
-            if cycle >= 20 && (cycle - 20) % 40 == 0 {
-                strength += cycle * reg;
-            }
-            let y = (cycle - 1) / 40;
-            let x = (cycle - 1) % 40;
-            if x == reg || x == reg - 1 || x == reg + 1 {
-                display[y as usize][x as usize] = b'#';
-            }
-            reg += num;
+            run_cycle(reg, &mut cycle, &mut strength, &mut display);
+            run_cycle(reg, &mut cycle, &mut strength, &mut display);
+            reg += num.parse::<i32>().unwrap();
         }
     });
 
     println!("Part 1: {}", strength);
-
     println!("Part 2:");
-    for row in display.iter() {
-        for col in row.iter() {
-            print!("{}", *col as char);
-        }
-        println!();
+    for row in display.chunks(40) {
+        println!("{}", std::str::from_utf8(row).unwrap());
     }
 }
