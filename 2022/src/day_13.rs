3c594ff0a287429e05fd1fe2d1651bdfce1d17bf
Indiana Kernick <indiana.kernick@gmail.com>
2022-12-13 16:15:11 +1030

    Day 13, part 2

--- 2022/src/day_13.rs
+++ 2022/src/day_13.rs
@@ -1,11 +1,12 @@
 use std::cmp::Ordering;
 
-enum Lists {
+#[derive(Clone)]
+enum Packet {
     Int(i32),
-    List(Vec<Lists>),
+    List(Vec<Packet>),
 }
 
-fn parse(line: &str) -> (Lists, &str) {
+fn parse(line: &str) -> (Packet, &str) {
     if let Some(mut line) = line.strip_prefix('[') {
         let mut list = Vec::new();
 
@@ -22,30 +23,57 @@ fn parse(line: &str) -> (Lists, &str) {
             }
         }
 
-        (Lists::List(list), line)
+        (Packet::List(list), line)
     } else {
         if let Some(end) = line.find(|c: char| !c.is_ascii_digit()) {
-            (Lists::Int(line[0..end].parse().unwrap()), &line[end..])
+            (Packet::Int(line[0..end].parse().unwrap()), &line[end..])
         } else {
-            (Lists::Int(line.parse().unwrap()), "")
+            (Packet::Int(line.parse().unwrap()), "")
         }
     }
 }
 
-fn compare(left: Lists, right: Lists) -> Ordering {
+fn compare(left: &Packet, right: &Packet) -> Ordering {
     match (left, right) {
-        (Lists::Int(l), Lists::Int(r)) => l.cmp(&r),
-        (Lists::Int(l), Lists::List(r)) => compare(Lists::List(vec![Lists::Int(l)]), Lists::List(r)),
-        (Lists::List(l), Lists::Int(r)) => compare(Lists::List(l), Lists::List(vec![Lists::Int(r)])),
-        (Lists::List(l), Lists::List(r)) => {
-            let len_cmp = l.len().cmp(&r.len());
-            for (li, ri) in l.into_iter().zip(r.into_iter()) {
-                let order = compare(li, ri);
+        (Packet::Int(l), Packet::Int(r)) => l.cmp(&r),
+        (Packet::Int(l), Packet::List(r)) => {
+            if r.is_empty() {
+                Ordering::Greater
+            } else {
+                let order = compare(&Packet::Int(*l), &r[0]);
+                if !order.is_eq() {
+                    return order;
+                }
+                if r.len() > 1 {
+                    Ordering::Less
+                } else {
+                    Ordering::Equal
+                }
+            }
+        },
+        (Packet::List(l), Packet::Int(r)) => {
+            if l.is_empty() {
+                Ordering::Less
+            } else {
+                let order = compare(&l[0], &Packet::Int(*r));
+                if !order.is_eq() {
+                    return order;
+                }
+                if l.len() > 1 {
+                    Ordering::Greater
+                } else {
+                    Ordering::Equal
+                }
+            }
+        },
+        (Packet::List(l), Packet::List(r)) => {
+            for i in 0..l.len().min(r.len()) {
+                let order = compare(&l[i], &r[i]);
                 if !order.is_eq() {
                     return order;
                 }
             }
-            len_cmp
+            l.len().cmp(&r.len())
         },
     }
 }
@@ -54,6 +82,7 @@ pub fn solve(input: &str) -> (usize, usize) {
     let mut line_iter = input.lines();
     let mut ordered_sum = 0;
     let mut pair_index = 1;
+    let mut packets = Vec::new();
 
     loop {
         let left = if let Some(l) = line_iter.next() {
@@ -67,12 +96,25 @@ pub fn solve(input: &str) -> (usize, usize) {
         let left = parse(left).0;
         let right = parse(right).0;
 
-        if compare(left, right) == Ordering::Less {
+        if compare(&left, &right) == Ordering::Less {
             ordered_sum += pair_index;
         }
+
+        packets.push(left);
+        packets.push(right);
 
         pair_index += 1;
     }
 
-    (ordered_sum, 0)
+    let decoder_a = Packet::List(vec![Packet::List(vec![Packet::Int(2)])]);
+    let decoder_b = Packet::List(vec![Packet::List(vec![Packet::Int(6)])]);
+
+    packets.push(decoder_a.clone());
+    packets.push(decoder_b.clone());
+    packets.sort_unstable_by(|a, b| compare(a, b));
+
+    let idx_a = 1 + packets.iter().position(|p| compare(&decoder_a, p).is_eq()).unwrap();
+    let idx_b = 1 + packets.iter().position(|p| compare(&decoder_b, p).is_eq()).unwrap();
+
+    (ordered_sum, idx_a * idx_b)
 }
