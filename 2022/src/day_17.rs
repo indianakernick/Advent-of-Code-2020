5c33eb60f1b25e368863bd93dbdcb169d149c71b
Indiana Kernick <indiana.kernick@gmail.com>
2022-12-17 18:15:19 +1030

    Day 17, part 2

--- 2022/src/day_17.rs
+++ 2022/src/day_17.rs
@@ -1,3 +1,5 @@
+use std::collections::{VecDeque, HashMap};
+
 const ROCK_0: [u8; 1] = [0b00111100];
 const ROCK_1: [u8; 3] = [
     0b00010000,
@@ -21,25 +23,56 @@ const ROCK_4: [u8; 2] = [
 ];
 const ROCKS: [&[u8]; 5] = [&ROCK_0, &ROCK_1, &ROCK_2, &ROCK_3, &ROCK_4];
 
-pub fn solve(input: &str) -> (usize, usize) {
+fn get_height(chamber: &VecDeque<u8>) -> usize {
+    chamber.iter().rposition(|row| *row != 0).map(|i| i + 1).unwrap_or(0)
+}
+
+fn solve_impl(input: &str, count: usize) -> usize {
     // positive is up
-    let mut chamber = Vec::<u8>::new();
+    let mut chamber = VecDeque::<u8>::new();
+    let mut trim_size = 0;
     let mut falling_rock_idx = 0;
     let mut falling_rock = Vec::<u8>::new();
     let mut falling_rock_y;
-    let mut jet = input.bytes().filter(|b| *b == b'<' || *b == b'>').cycle();
+    let mut jet_idx = 0;
+    let jet = &input.as_bytes()[0..input.bytes().rposition(|b| b == b'<' || b == b'>').unwrap() + 1];
 
-    for _ in 0..2022 {
+    let mut chambers = HashMap::<(VecDeque<u8>, usize, usize), (usize, usize)>::new();
+    let mut r = 0;
+    let mut found_cycle = false;
+
+    while r < count {
         falling_rock.clear();
         falling_rock.extend_from_slice(ROCKS[falling_rock_idx]);
 
-        let base_size = chamber.iter().rposition(|row| *row != 0).map(|i| i + 1).unwrap_or(0);
+        let unused_size = chamber.iter().rposition(|row| *row == 0b11111110).map(|i| i + 1);
+        if let Some(unused_size) = unused_size {
+            trim_size += unused_size;
+            chamber.drain(0..unused_size);
+            let base_size = get_height(&chamber);
+
+            chamber.truncate(base_size);
+            let insert = chambers.insert((chamber.clone(), falling_rock_idx, jet_idx), (r, trim_size));
+            if !found_cycle && insert.is_some() {
+                // We've been in this state before. Repeat the cycle until we're
+                // near the end.
+                found_cycle = true;
+                let (start, start_trim) = insert.unwrap();
+                let cycle_size = r - start;
+                let cycles = (count - r) / cycle_size;
+                r += cycle_size * cycles;
+                trim_size += (trim_size - start_trim) * cycles;
+            }
+        }
+
+        let base_size = get_height(&chamber);
         falling_rock_y = base_size + 3;
         let new_size = falling_rock_y + falling_rock.len();
         chamber.resize(new_size, 0);
 
-        loop {
-            let dir = jet.next().unwrap();
+        'fall: loop {
+            let dir = jet[jet_idx];
+            jet_idx = (jet_idx + 1) % jet.len();
 
             match dir {
                 b'<' => {
@@ -90,19 +123,14 @@ pub fn solve(input: &str) -> (usize, usize) {
                 break;
             }
 
-            let mut collide = false;
             for (y, row) in falling_rock.iter().enumerate() {
                 if (row & chamber[falling_rock_y + y - 1]) != 0 {
-                    // hitting another rock
-                    collide = true;
-                    break;
+                    // hitting another rock. stop falling
+                    break 'fall;
                 }
             }
-            if !collide {
-                falling_rock_y -= 1;
-            } else {
-                break;
-            }
+
+            falling_rock_y -= 1;
         }
 
         // coming to rest
@@ -111,9 +139,21 @@ pub fn solve(input: &str) -> (usize, usize) {
         }
 
         falling_rock_idx = (falling_rock_idx + 1) % ROCKS.len();
+        r += 1;
     }
 
-    let count = chamber.iter().rposition(|row| *row != 0).map(|i| i + 1).unwrap();
+    trim_size + get_height(&chamber)
+}
 
-    (count, 0)
+pub fn solve(input: &str) -> (usize, usize) {
+    (solve_impl(input, 2022), solve_impl(input, 1000000000000))
+}
+
+#[cfg(test)]
+#[test]
+fn example() {
+    let input = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>";
+    // It doesn't find a cycle...
+    assert_eq!(solve_impl(input, 2022), 3068);
+    // assert_eq!(solve_impl(input, 1000000000000), 1514285714288);
 }
