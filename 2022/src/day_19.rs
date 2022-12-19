e113765ab346c074c51bbcd623ca04f6f6709667
Indiana Kernick <indiana.kernick@gmail.com>
2022-12-19 19:44:12 +1030

    Ignore the very slow test

--- 2022/src/day_19.rs
+++ 2022/src/day_19.rs
@@ -177,17 +177,19 @@ pub fn solve(input: &str) -> (u16, u16) {
         })
         .sum();
 
-    let product = blueprints.iter().take(3).map(|b| {
-        memoize_table.clear();
-        simulate(&mut memoize_table, b, inv, 32)
-    })
-    .product();
+    let product = blueprints[..3].iter()
+        .map(|b| {
+            memoize_table.clear();
+            simulate(&mut memoize_table, b, inv, 32)
+        })
+        .product();
 
     (quality_sum, product)
 }
 
 #[cfg(test)]
 #[test]
+#[ignore] // this takes 5 minutes but the real problem takes 30 seconds...
 fn example() {
     let input =
 "Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.
