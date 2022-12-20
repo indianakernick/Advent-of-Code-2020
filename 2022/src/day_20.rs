1433fe7c53fa3b698bae780aa1db746e7ba584b2
Indiana Kernick <indiana.kernick@gmail.com>
2022-12-20 17:19:34 +1030

    Clean up day 20

--- 2022/src/day_20.rs
+++ 2022/src/day_20.rs
@@ -12,10 +12,10 @@ fn mix(nums: &mut Vec<(usize, i64)>) {
         let mut new_index = old_index as i64 + pair.1;
 
         if new_index <= 0 {
-            new_index += (1 + (-new_index / (len - 1))) * (len - 1);
+            new_index += (1 + -new_index / (len - 1)) * (len - 1);
         }
         if new_index >= len {
-            new_index -= (0 + (new_index / (len - 1))) * (len - 1);
+            new_index -= new_index / (len - 1) * (len - 1);
         }
 
         let new_index = new_index as usize;
@@ -30,28 +30,25 @@ fn mix(nums: &mut Vec<(usize, i64)>) {
             nums.copy_within(new_index..old_index, new_index + 1);
         }
 
-        nums[new_index as usize] = pair;
+        nums[new_index] = pair;
     }
 }
 
 fn get_coord_sum(nums: &[(usize, i64)]) -> i64 {
-    let zero_index = nums.iter().position(|(_, value)| *value == 0).unwrap();
-    let first = nums[(zero_index + 1000) % nums.len()].1;
-    let second = nums[(zero_index + 2000) % nums.len()].1;
-    let third = nums[(zero_index + 3000) % nums.len()].1;
-    first + second + third
+    let zero = nums.iter().position(|(_, value)| *value == 0).unwrap();
+    [1000, 2000, 3000].iter().map(|off| nums[(zero + off) % nums.len()].1).sum()
 }
 
 pub fn solve(input: &str) -> (i64, i64) {
-    let mut nums = Vec::<(usize, i64)>::new();
+    let mut nums_1 = Vec::<(usize, i64)>::new();
 
     for line in input.lines() {
-        nums.push((nums.len(), line.parse().unwrap()));
+        nums_1.push((nums_1.len(), line.parse().unwrap()));
     }
 
-    let mut nums_2 = nums.clone();
+    let mut nums_2 = nums_1.clone();
 
-    mix(&mut nums);
+    mix(&mut nums_1);
 
     for n in nums_2.iter_mut() {
         n.1 *= 811589153;
@@ -61,5 +58,21 @@ pub fn solve(input: &str) -> (i64, i64) {
         mix(&mut nums_2);
     }
 
-    (get_coord_sum(&nums), get_coord_sum(&nums_2))
+    (get_coord_sum(&nums_1), get_coord_sum(&nums_2))
+}
+
+#[cfg(test)]
+#[test]
+fn example() {
+    let input =
+"1
+2
+-3
+3
+-2
+0
+4";
+    let output = solve(input);
+    assert_eq!(output.0, 3);
+    assert_eq!(output.1, 1623178306);
 }
