e567d38ea9472d23c090afeba48250b74c08fe53
Indiana Kernick <indiana.kernick@gmail.com>
2023-12-01 16:19:08 +1030

    Clean up day 1

--- 2023/src/day_01.rs
+++ 2023/src/day_01.rs
@@ -1,71 +1,75 @@
 pub fn solve(input: &str) -> (u32, u32) {
-    let mut sum_1 = 0;
-    let mut sum_2 = 0;
+    let mut sum = (0, 0);
 
     for line in input.lines() {
-        let mut first_1 = None;
-        let mut last_1 = None;
-        let mut first_2 = None;
-        let mut last_2 = None;
+        sum.0 += first_last_sum::<false>(line);
+        sum.1 += first_last_sum::<true>(line);
+    }
 
-        for i in 0..line.len() {
-            let char = line.as_bytes()[i];
+    sum
+}
 
-            let mut digit = None;
+const WORDS: [&str; 9] = [
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+];
 
-            if char.is_ascii_digit() {
-                digit = Some(char);
-                if first_1.is_none() {
-                    first_1 = Some(char);
+fn first_last_sum<const USE_WORDS: bool>(line: &str) -> u32 {
+    let mut first = None;
+    let mut last = None;
+
+    for (char_idx, char) in line.as_bytes().iter().enumerate() {
+        let mut digit = None;
+
+        if char.is_ascii_digit() {
+            digit = Some(char - b'0');
+        } else if USE_WORDS {
+            for (word_idx, word) in WORDS.iter().enumerate() {
+                if line[char_idx..].starts_with(word) {
+                    digit = Some(word_idx as u8 + 1);
                 }
-                last_1 = Some(char);
-            }
-
-            if line[i..].starts_with("one") {
-                digit = Some(b'1');
-            }
-            if line[i..].starts_with("two") {
-                digit = Some(b'2');
-            }
-            if line[i..].starts_with("three") {
-                digit = Some(b'3');
-            }
-            if line[i..].starts_with("four") {
-                digit = Some(b'4');
-            }
-            if line[i..].starts_with("five") {
-                digit = Some(b'5');
-            }
-            if line[i..].starts_with("six") {
-                digit = Some(b'6');
-            }
-            if line[i..].starts_with("seven") {
-                digit = Some(b'7');
-            }
-            if line[i..].starts_with("eight") {
-                digit = Some(b'8');
-            }
-            if line[i..].starts_with("nine") {
-                digit = Some(b'9');
-            }
-
-            if let Some(digit) = digit {
-                if first_2.is_none() {
-                    first_2 = Some(digit);
-                }
-                last_2 = Some(digit);
             }
         }
 
-        sum_1 += String::from_utf8(vec![first_1.unwrap(), last_1.unwrap()])
-            .unwrap()
-            .parse::<u32>()
-            .unwrap();
-        sum_2 += String::from_utf8(vec![first_2.unwrap(), last_2.unwrap()])
-            .unwrap()
-            .parse::<u32>()
-            .unwrap();
+        if let Some(found) = digit {
+            first.get_or_insert(found);
+            last = digit;
+        }
     }
 
-    (sum_1, sum_2)
+    (first.unwrap_or(0) * 10 + last.unwrap_or(0)) as u32
+}
+
+#[cfg(test)]
+#[test]
+fn example_1() {
+    let input =
+"1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet";
+    let output = solve(input);
+    assert_eq!(output.0, 142);
+}
+
+#[cfg(test)]
+#[test]
+fn example_2() {
+    let input =
+"two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen";
+    let output = solve(input);
+    assert_eq!(output.1, 281);
 }
