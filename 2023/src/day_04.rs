be05fae84892d25c01a15a9df8deca9ba233c0b4
Indiana Kernick <indiana.kernick@gmail.com>
2023-12-04 16:38:47 +1030

    Clean up day 4

--- 2023/src/day_04.rs
+++ 2023/src/day_04.rs
@@ -3,70 +3,76 @@ use std::collections::HashSet;
 pub fn solve(input: &str) -> (u32, u32) {
     let mut points_sum = 0;
     let mut winning_set = HashSet::new();
-    let mut copy_card_counts = Vec::<u32>::new();
-    let mut original_card_count = 0;
+    let mut card_copy_counts = Vec::<u32>::new();
+    let mut card_index = 0;
 
     for line in input.lines() {
-        let bytes = line.as_bytes();
-
-        let colon = bytes.iter().position(|b| *b == b':').unwrap();
-        let mut index = colon + 2;
-
-        while bytes[index] != b'|' {
-            winning_set.insert(get_value(&bytes[index..]));
-            index += 3;
-        }
-
-        index += 2;
-
-        let mut winning_count = 0;
-
-        while index < bytes.len() {
-            if winning_set.contains(&get_value(&bytes[index..])) {
-                winning_count += 1;
-            }
-
-            index += 3;
-        }
-
-        winning_set.clear();
+        let winning_count = count_winners(line.as_bytes(), &mut winning_set);
 
         if winning_count != 0 {
             points_sum += 1 << (winning_count - 1);
         }
 
-        let new_length = original_card_count + winning_count + 1;
+        let new_length = card_index + winning_count + 1;
 
-        if new_length > copy_card_counts.len() {
-            copy_card_counts.reserve(new_length - copy_card_counts.len());
+        if new_length > card_copy_counts.len() {
+            card_copy_counts.reserve(new_length - card_copy_counts.len());
         }
 
-        let multiplier = if original_card_count < copy_card_counts.len() {
-            1 + copy_card_counts[original_card_count]
+        let total_count = if card_index < card_copy_counts.len() {
+            1 + card_copy_counts[card_index]
         } else {
             1
         };
 
         for card in 0..winning_count {
-            let card_index = original_card_count + card + 1;
+            let copy_index = card_index + card + 1;
 
-            if card_index < copy_card_counts.len() {
-                copy_card_counts[card_index] += multiplier;
+            if copy_index < card_copy_counts.len() {
+                card_copy_counts[copy_index] += total_count;
                 continue;
             }
-            if card_index > copy_card_counts.len() {
-                copy_card_counts.push(0);
+
+            if copy_index > card_copy_counts.len() {
+                card_copy_counts.push(0);
             }
-            copy_card_counts.push(multiplier);
+
+            card_copy_counts.push(total_count);
         }
 
-        original_card_count += 1;
+        card_index += 1;
     }
 
-    (points_sum, copy_card_counts.iter().sum::<u32>() + original_card_count as u32)
+    (points_sum, card_copy_counts.iter().sum::<u32>() + card_index as u32)
 }
 
-fn get_value(s: &[u8]) -> u8 {
+fn count_winners(line: &[u8], winning_set: &mut HashSet<u8>) -> usize {
+    let colon = line.iter().position(|b| *b == b':').unwrap();
+    let mut index = colon + 2;
+
+    while line[index] != b'|' {
+        winning_set.insert(parse_value(&line[index..]));
+        index += 3;
+    }
+
+    index += 2;
+
+    let mut winning_count = 0;
+
+    while index < line.len() {
+        if winning_set.contains(&parse_value(&line[index..])) {
+            winning_count += 1;
+        }
+
+        index += 3;
+    }
+
+    winning_set.clear();
+
+    winning_count
+}
+
+fn parse_value(s: &[u8]) -> u8 {
     let mut value = 0;
 
     if s[0] != b' ' {
