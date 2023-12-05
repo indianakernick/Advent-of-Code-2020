bdad3071a010d3cee54fd6bdb37d9e17bf2fc899
Indiana Kernick <indiana.kernick@gmail.com>
2023-12-05 16:29:27 +1030

    Clean up day 5

--- 2023/src/day_05.rs
+++ 2023/src/day_05.rs
@@ -7,7 +7,7 @@ pub fn solve(input: &str) -> (u64, u64) {
 
     lines.next().unwrap();
 
-    'outer: loop {
+    'mapping: loop {
         let mut ranges = Vec::new();
 
         lines.next().unwrap();
@@ -22,25 +22,24 @@ pub fn solve(input: &str) -> (u64, u64) {
                 }
             } else {
                 mappings.push(ranges);
-                break 'outer;
+                break 'mapping;
             }
         }
     }
 
-    let mut min_location_1 = u64::MAX;
-    let mut min_location_2 = u64::MAX;
+    let mut min = (u64::MAX, u64::MAX);
 
     for seed in seeds.iter() {
-        min_location_1 = min_location_1.min(map_value(*seed, &mappings));
+        min.0 = min.0.min(map_value(*seed, &mappings));
     }
 
     for pair in seeds.chunks(2) {
         for seed in pair[0]..pair[0] + pair[1] {
-            min_location_2 = min_location_2.min(map_value(seed, &mappings));
+            min.1 = min.1.min(map_value(seed, &mappings));
         }
     }
 
-    (min_location_1, min_location_2)
+    min
 }
 
 fn map_value(mut value: u64, mappings: &Vec<Vec<MapRange>>) -> u64 {
@@ -93,9 +92,50 @@ fn parse_number(s: &[u8]) -> u64 {
         .sum()
 }
 
-#[derive(Debug)]
 struct MapRange {
     destination_start: u64,
     source_start: u64,
     length: u64,
+}
+
+#[cfg(test)]
+#[test]
+fn example() {
+    let input =
+"seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4";
+    let output = solve(input);
+    assert_eq!(output.0, 35);
+    assert_eq!(output.1, 46);
 }
