1f60b9b4cbd9753938c512347480d6ef775ac9e0
Indiana Kernick <indiana.kernick@gmail.com>
2023-12-07 16:53:10 +1030

    Clean up day 7

--- 2023/src/day_07.rs
+++ 2023/src/day_07.rs
@@ -1,46 +1,28 @@
-use std::{collections::HashMap, cmp::Ordering};
+use std::cmp::Ordering;
 
 use crate::common;
 
 pub fn solve(input: &str) -> (u32, u32) {
-    let mut hands = Vec::new();
+    let mut hands = common::lines_iter(input)
+        .map(|line| (
+            Hand::try_from(&line[..HAND_SIZE]).unwrap(),
+            common::parse_u32(&line[6..])
+        ))
+        .collect::<Vec<_>>();
 
-    for line in common::lines_iter(input) {
-        hands.push((&line[..5], common::parse_u32(&line[6..])));
-    }
-
-    hands.sort_by(|(a, _), (b, _)| compare::<false>(a, b));
-
+    hands.sort_unstable_by(|(a, _), (b, _)| compare::<false>(a, b));
     let score_1 = score_sum(&hands);
 
-    hands.sort_by(|(a, _), (b, _)| compare::<true>(a, b));
-
+    hands.sort_unstable_by(|(a, _), (b, _)| compare::<true>(a, b));
     let score_2 = score_sum(&hands);
 
     (score_1, score_2)
 }
 
-fn compare<const WILD: bool>(a_hand: &[u8], b_hand: &[u8]) -> Ordering {
-    let a_type = classify::<WILD>(a_hand);
-    let b_type = classify::<WILD>(b_hand);
+const HAND_SIZE: usize = 5;
+type Hand<'a> = &'a [u8; HAND_SIZE];
 
-    if a_type != b_type {
-        return a_type.cmp(&b_type);
-    }
-
-    for (a_card, b_card) in a_hand.iter().zip(b_hand.iter()) {
-        let a_strength = card_strength::<WILD>(*a_card);
-        let b_strength = card_strength::<WILD>(*b_card);
-
-        if a_strength != b_strength {
-            return a_strength.cmp(&b_strength);
-        }
-    }
-
-    Ordering::Equal
-}
-
-fn score_sum(hands: &Vec<(&[u8], u32)>) -> u32 {
+fn score_sum(hands: &Vec<(Hand, u32)>) -> u32 {
     hands
         .iter()
         .enumerate()
@@ -48,53 +30,102 @@ fn score_sum(hands: &Vec<(&[u8], u32)>) -> u32 {
         .sum()
 }
 
-fn classify<const WILD: bool>(hand: &[u8]) -> u32 {
-    let mut map = HashMap::new();
+fn compare<const WILD: bool>(a_hand: Hand, b_hand: Hand) -> Ordering {
+    let a_type = hand_type::<WILD>(a_hand);
+    let b_type = hand_type::<WILD>(b_hand);
+    let cmp_type = a_type.cmp(&b_type);
 
-    for card in hand.iter() {
-        map.entry(*card).and_modify(|count| *count += 1).or_insert(1u32);
+    if !cmp_type.is_eq() {
+        return cmp_type;
     }
 
-    let mut counts = map.iter().map(|(card, count)| (*card, *count)).collect::<Vec<_>>();
-    counts.sort_unstable_by_key(|(_, count)| *count);
+    for (a_card, b_card) in a_hand.iter().zip(b_hand.iter()) {
+        let a_strength = card_strength::<WILD>(*a_card);
+        let b_strength = card_strength::<WILD>(*b_card);
+        let cmp_strength = a_strength.cmp(&b_strength);
 
-    if WILD && counts.len() > 1 {
-        let wild_index = counts.iter().position(|(card, _)| *card == b'J');
-        if let Some(index) = wild_index {
-            let count = counts.remove(index).1;
-            let last = counts.len() - 1;
-            counts[last].1 += count;
+        if !cmp_strength.is_eq() {
+            return cmp_strength;
         }
     }
 
-    if counts.len() == 1 {
-        return 6; // five of a kind
-    } else if counts.len() == 2 {
-        if counts[0].1 == 1 {
-            return 5; // four of a kind
-        } else {
-            return 4; // full house
+    Ordering::Equal
+}
+
+fn hand_type<const WILD: bool>(hand: Hand) -> HandType {
+    let mut card_counts: [(u8, u8); HAND_SIZE] = [(0, 0); HAND_SIZE];
+    let mut length = 0;
+
+    for hand_card in hand.iter() {
+        for (card, count) in card_counts.iter_mut() {
+            if *card == 0 {
+                *card = *hand_card;
+                *count = 1;
+                length += 1;
+                break;
+            } else if *card == *hand_card {
+                *count += 1;
+                break;
+            }
         }
-    } else if counts.len() == 3 {
-        if counts[0].1 == 1 && counts[1].1 == 1 {
-            return 3; // three of a kind
-        } else {
-            return 2; // two pair
+    }
+
+    card_counts[0..length].sort_unstable_by_key(|(_, count)| *count);
+
+    if WILD && length > 1 {
+        let index = card_counts.iter().position(|(card, _)| *card == WILD_CARD);
+
+        if let Some(index) = index {
+            length -= 1;
+            card_counts[length].1 += card_counts[index].1;
+
+            if index < length {
+                card_counts.copy_within(index + 1..length + 1, index);
+            }
         }
-    } else if counts.len() == 4 {
-        return 1; // one pair
+    }
+
+    if length == 1 {
+        HandType::FiveOfAKind
+    } else if length == 2 {
+        if card_counts[0].1 == 1 {
+            HandType::FourOfAKind
+        } else {
+            HandType::FullHouse
+        }
+    } else if length == 3 {
+        if card_counts[0].1 == 1 && card_counts[1].1 == 1 {
+            HandType::ThreeOfAKind
+        } else {
+            HandType::TwoPair
+        }
+    } else if length == 4 {
+        HandType::OnePair
     } else {
-        return 0;
+        HandType::HighCard
     }
 }
 
-fn card_strength<const WILD: bool>(card: u8) -> usize {
-    if WILD && card == b'J' {
+#[derive(PartialEq, Eq, PartialOrd, Ord)]
+enum HandType {
+    HighCard,
+    OnePair,
+    TwoPair,
+    ThreeOfAKind,
+    FullHouse,
+    FourOfAKind,
+    FiveOfAKind,
+}
+
+fn card_strength<const WILD: bool>(card: u8) -> u8 {
+    if WILD && card == WILD_CARD {
         0
     } else {
-        CARD_STRENGTH.iter().position(|c| *c == card).unwrap() + 1
+        CARD_STRENGTH.iter().position(|c| *c == card).unwrap() as u8 + 1
     }
 }
+
+const WILD_CARD: u8 = b'J';
 
 const CARD_STRENGTH: [u8; 13] = [
     b'2',
@@ -111,3 +142,17 @@ const CARD_STRENGTH: [u8; 13] = [
     b'K',
     b'A',
 ];
+
+#[cfg(test)]
+#[test]
+fn example() {
+    let input =
+"32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483";
+    let output = solve(input);
+    assert_eq!(output.0, 6440);
+    assert_eq!(output.1, 5905);
+}
