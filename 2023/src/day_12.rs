6bec5ee635a19a4367154f29dad97607b8b21a12
Indiana Kernick <indiana.kernick@gmail.com>
2023-12-14 12:20:59 +1030

    Day 12, part 2

--- 2023/src/day_12.rs
+++ 2023/src/day_12.rs
@@ -1,15 +1,17 @@
+use std::collections::HashMap;
+
 use crate::common;
 
-pub fn solve(input: &str) -> (u32, u32) {
-    let mut sum = 0;
-    let mut arrangement_spec = Vec::new();
-    let mut expected_group_sizes = Vec::new();
+pub fn solve(input: &str) -> (u64, u64) {
+    let mut arrangement_specs = Vec::new();
+    let mut group_sizes = Vec::new();
+
+    const COPY: usize = 5;
 
     for line in common::lines_iter(input) {
         let space = line.iter().position(|ch| *ch == b' ').unwrap();
 
-        arrangement_spec.clear();
-        arrangement_spec.push(line[0]);
+        let mut arrangement_spec = vec![line[0]];
 
         // Collapse consecutive operational springs.
         for i in 1..space {
@@ -19,62 +21,106 @@ pub fn solve(input: &str) -> (u32, u32) {
             arrangement_spec.push(line[i]);
         }
 
-        expected_group_sizes.clear();
-        expected_group_sizes.extend(line[space + 1..]
-            .split(|ch| *ch == b',')
-            .map(common::parse_u32));
+        let spec_len = arrangement_spec.len();
 
-        fn inner(spec: &[u8], sizes: &[u32]) -> u32 {
-            if sizes.len() == 0 {
-                return if spec.iter().all(|b| *b == b'.' || *b == b'?') {
-                    // Assume that the remaining unknowns are operational.
-                    1
-                } else {
-                    0
-                };
-            }
-
-            if (spec.len() == 0 && sizes.len() > 0) || spec.len() < sizes[0] as usize {
-                return 0;
-            }
-
-            if !spec[0..sizes[0] as usize].iter().all(|b| *b == b'#' || *b == b'?') {
-                return if spec[0] == b'.' || spec[0] == b'?' {
-                    // Assume that the first spring is operational.
-                    inner(&spec[1..], sizes)
-                } else {
-                    0
-                };
-            }
-
-            if spec.len() > sizes[0] as usize {
-                let next = spec[sizes[0] as usize];
-
-                let first = if next == b'.' || next == b'?' {
-                    // Assume that spring after this contiguous block of broken
-                    // springs is operational.
-                    inner(&spec[sizes[0] as usize + 1..], &sizes[1..])
-                } else {
-                    0
-                };
-
-                let second = if spec[0] == b'?' {
-                    // Assume that the first spring is operational.
-                    inner(&spec[1..], sizes)
-                } else {
-                    0
-                };
-
-                return first + second;
-            }
-
-            inner(&spec[sizes[0] as usize..], &sizes[1..])
+        for _ in 1..COPY {
+            arrangement_spec.push(b'?');
+            arrangement_spec.extend_from_within(..spec_len);
         }
 
-        sum += inner(&arrangement_spec, &expected_group_sizes);
+        arrangement_specs.push(arrangement_spec);
+
+        let mut group_size = line[space + 1..]
+            .split(|ch| *ch == b',')
+            .map(common::parse_u64)
+            .collect::<Vec<_>>();
+
+        let group_size_len = group_size.len();
+
+        for _ in 1..COPY {
+            group_size.extend_from_within(..group_size_len);
+        }
+
+        group_sizes.push(group_size);
     }
 
-    (sum, 0)
+    let length = arrangement_specs.len();
+    let mut sum = (0, 0);
+    let mut cache = HashMap::new();
+
+    for i in 0..length {
+        let spec = &arrangement_specs[i];
+        let sizes = &group_sizes[i];
+        sum.0 += count_arrangements(
+            &mut cache,
+            &spec[..spec.len() / COPY],
+            &sizes[..sizes.len() / COPY]
+        );
+        sum.1 += count_arrangements(&mut cache, spec, sizes);
+    }
+
+    sum
+}
+
+fn count_arrangements<'a, 'b>(
+    cache: &'a mut HashMap<(&'b [u8], &'b [u64]), u64>,
+    spec: &'b [u8],
+    sizes: &'b [u64],
+) -> u64 {
+    if sizes.len() == 0 {
+        return if spec.iter().all(|b| *b == b'.' || *b == b'?') {
+            // Assume that the remaining unknowns are operational.
+            1
+        } else {
+            0
+        };
+    }
+
+    if (spec.len() == 0 && sizes.len() > 0) || spec.len() < sizes[0] as usize {
+        return 0;
+    }
+
+    if let Some(count) = cache.get(&(spec, sizes)) {
+        return *count;
+    }
+
+    if !spec[0..sizes[0] as usize].iter().all(|b| *b == b'#' || *b == b'?') {
+        let result = if spec[0] == b'.' || spec[0] == b'?' {
+            // Assume that the first spring is operational.
+            count_arrangements(cache, &spec[1..], sizes)
+        } else {
+            0
+        };
+        cache.insert((spec, sizes), result);
+        return result;
+    }
+
+    if spec.len() > sizes[0] as usize {
+        let next = spec[sizes[0] as usize];
+
+        let first = if next == b'.' || next == b'?' {
+            // Assume that spring after this contiguous block of broken
+            // springs is operational.
+            count_arrangements(cache, &spec[sizes[0] as usize + 1..], &sizes[1..])
+        } else {
+            0
+        };
+
+        let second = if spec[0] == b'?' {
+            // Assume that the first spring is operational.
+            count_arrangements(cache, &spec[1..], sizes)
+        } else {
+            0
+        };
+
+        let result = first + second;
+        cache.insert((spec, sizes), result);
+        return result;
+    }
+
+    let result = count_arrangements(cache, &spec[sizes[0] as usize..], &sizes[1..]);
+    cache.insert((spec, sizes), result);
+    result
 }
 
 #[cfg(test)]
@@ -89,4 +135,5 @@ fn example() {
 ?###???????? 3,2,1";
     let output = solve(input);
     assert_eq!(output.0, 21);
+    assert_eq!(output.1, 525152);
 }
