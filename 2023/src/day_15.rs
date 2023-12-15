df94e6b416bfc9ba660480c06c654fbb57aca230
Indiana Kernick <indiana.kernick@gmail.com>
2023-12-15 16:41:17 +1030

    Clean up day 15

--- 2023/src/day_15.rs
+++ 2023/src/day_15.rs
@@ -1,4 +1,4 @@
-pub fn solve(input: &str) -> (u32, usize) {
+pub fn solve(input: &str) -> (u32, u32) {
     let bytes = input.as_bytes();
     let bytes = if bytes[bytes.len() - 1] == b'\n' {
         &bytes[..bytes.len() - 1]
@@ -14,7 +14,10 @@ pub fn solve(input: &str) -> (u32, usize) {
     let mut boxes: [Vec<(&[u8], u8)>; 256] = std::array::from_fn(|_| Vec::new());
 
     for step in bytes.split(|b| *b == b',') {
-        let operation_index = step.iter().position(|b| *b == b'=' || *b == b'-').unwrap();
+        let operation_index = step
+            .iter()
+            .position(|b| *b == b'=' || *b == b'-')
+            .unwrap();
         let label = &step[..operation_index];
         let box_index = hash(label);
         let r#box = &mut boxes[box_index as usize];
@@ -39,7 +42,9 @@ pub fn solve(input: &str) -> (u32, usize) {
     for box_index in 0..boxes.len() {
         let r#box = &boxes[box_index];
         for lens_index in 0..r#box.len() {
-            focusing_power_sum += (box_index + 1) * (lens_index + 1) * (r#box[lens_index].1 - b'0') as usize;
+            focusing_power_sum += (box_index + 1) as u32
+                * (lens_index + 1) as u32
+                * (r#box[lens_index].1 - b'0') as u32;
         }
     }
 
@@ -54,4 +59,13 @@ fn hash(bytes: &[u8]) -> u8 {
     }
 
     value
+}
+
+#[cfg(test)]
+#[test]
+fn example() {
+    let input = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7";
+    let output = solve(input);
+    assert_eq!(output.0, 1320);
+    assert_eq!(output.1, 145);
 }
