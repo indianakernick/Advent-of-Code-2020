b2b0cd393935d168014829dcd323474935afad54
Indiana Kernick <indiana.kernick@gmail.com>
2024-05-08 21:17:23 +0930

    Use sets to attempt day 21, part 2
    This seems to be correct but is way too slow

--- 2023/src/day_21.rs
+++ 2023/src/day_21.rs
@@ -1,43 +1,47 @@
-use crate::common::Grid;
+use std::collections::HashSet;
 
-pub fn solve(input: &str) -> (usize, u64) {
-    count_reachable(input, 64)
+use crate::common::{self, Dir, Grid};
+
+pub fn solve(input: &str) -> (usize, usize) {
+    let input_grid = Grid::<Tile>::from_input(&input);
+    (count_reachable(input_grid, 64), count_reachable(input_grid, 26501365))
 }
 
-fn count_reachable(input: &str, steps: u32) -> (usize, u64) {
-    let input_grid = Grid::<Tile>::from_input(&input);
-    let stride = input_grid.get_stride();
+fn count_reachable(input_grid: Grid::<Tile>, steps: u32) -> usize {
+    let mut read_set = HashSet::new();
+    let mut write_set = HashSet::new();
 
-    let mut read_vec = input_grid.to_vec();
-    let mut write_vec = input_grid.to_vec();
+    read_set.insert(input_grid.pos_of(Tile::Start).unwrap());
 
     for _ in 0..steps {
-        for idx in 0..read_vec.len() {
-            if let Tile::Newline | Tile::Rock = write_vec[idx] {
-                continue;
-            }
+        for read_pos in read_set.iter() {
+            for dir in Dir::ALL {
+                let next = common::add(*read_pos, dir.to_vec());
+                let mut normalised_next = next;
 
-            let sides = [idx.wrapping_sub(stride), idx + 1, idx + stride, idx.wrapping_sub(1)];
-            let mut reachable = false;
+                if normalised_next.0 < 0 {
+                    normalised_next.0 += input_grid.get_width()
+                        * (1 + -normalised_next.0 / input_grid.get_width());
+                }
+                if normalised_next.1 < 0 {
+                    normalised_next.1 += input_grid.get_height()
+                        * (1 + -normalised_next.1 / input_grid.get_height());
+                }
 
-            for side in sides {
-                if side >= read_vec.len() { continue; }
+                normalised_next.0 %= input_grid.get_width();
+                normalised_next.1 %= input_grid.get_height();
 
-                if let Tile::Start | Tile::Reachable = read_vec[side] {
-                    reachable = true;
-                    break;
+                if input_grid.get(normalised_next) != Tile::Rock {
+                    write_set.insert(next);
                 }
             }
-
-            write_vec[idx] = if reachable { Tile::Reachable } else { Tile::Garden };
         }
 
-        std::mem::swap(&mut read_vec, &mut write_vec);
+        std::mem::swap(&mut read_set, &mut write_set);
+        write_set.clear();
     }
 
-    let count = read_vec.iter().filter(|tile| **tile == Tile::Reachable).count();
-
-    (count, 0)
+    read_set.len()
 }
 
 #[repr(u8)]
@@ -65,6 +69,12 @@ fn example() {
 .##.#.####.
 .##..##.##.
 ...........";
-    let output = count_reachable(input, 6);
-    assert_eq!(output.0, 16);
+    let input_grid = Grid::<Tile>::from_input(&input);
+    assert_eq!(count_reachable(input_grid, 6), 16);
+    assert_eq!(count_reachable(input_grid, 10), 50);
+    assert_eq!(count_reachable(input_grid, 50), 1594);
+    assert_eq!(count_reachable(input_grid, 100), 6536);
+    assert_eq!(count_reachable(input_grid, 500), 167004);
+    assert_eq!(count_reachable(input_grid, 1000), 668697);
+    assert_eq!(count_reachable(input_grid, 5000), 16733044);
 }
